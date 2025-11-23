5a2f2ee1d6c39d0adee457e91497b0562206239a
Ruben Bakker <ruben@uncomplex.ch>
2025-11-23 16:30:28 +0100

    codecrafters submit [skip ci]

--- src/main.ml
+++ src/main.ml
@@ -1,30 +1,32 @@
 open Unix
 
-let process_client client_socket =
+let rec process_client client_socket =
   let data = "+PONG\r\n" in
   let buf = Bytes.create 32 in
-  let bytes_sent = ref 1 in
-  while !bytes_sent > 0 do
-    let _ = read client_socket buf 0 16 in
-    bytes_sent :=
-      write client_socket (Bytes.of_string data) 0 (String.length data);
-    Printf.eprintf "bytes_sent %d" !bytes_sent
-  done;
-  close client_socket
+  let _ = read client_socket buf 0 16 in
+  let _ = write client_socket (Bytes.of_string data) 0 (String.length data) in
+  process_client client_socket
+
+let run_and_close_client client_socket =
+  let finally () = close client_socket in
+  let work () = process_client client_socket in
+  Fun.protect ~finally work
+
+let rec accept_loop server_socket =
+  let client_socket, _ = accept server_socket in
+  let _ = Thread.create run_and_close_client client_socket in
+  accept_loop server_socket
 
 let () =
   (* Create a TCP server socket *)
   let server_socket = socket PF_INET SOCK_STREAM 0 in
   setsockopt server_socket SO_REUSEADDR true;
   bind server_socket (ADDR_INET (inet_addr_of_string "127.0.0.1", 6379));
-  let client_threads = ref [] in
   listen server_socket 1;
 
-  (* Uncomment the code below to pass the first stage *)
-  while true do
-    let client_socket, _ = accept server_socket in
-    Printf.eprintf "starting new thread %d" (List.length !client_threads);
-    client_threads :=
-      Thread.create process_client client_socket :: !client_threads
-  done
+  try accept_loop server_socket
+  with e ->
+    close server_socket;
+    raise e
+
 (* close server_socket *)
