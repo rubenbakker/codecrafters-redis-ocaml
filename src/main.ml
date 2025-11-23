05026760cbf90f235fb639bb56c28590fe1e9928
Ruben Bakker <ruben@uncomplex.ch>
2025-11-23 16:11:10 +0100

    multiple clients

--- src/main.ml
+++ src/main.ml
@@ -1,20 +1,28 @@
 open Unix
 
+let process_client client_socket =
+  let data = "+PONG\r\n" in
+  let buf = Bytes.create 32 in
+  let bytes_sent = ref 1 in
+  while true do
+    let _ = read client_socket buf 0 16 in
+    bytes_sent :=
+      write client_socket (Bytes.of_string data) 0 (String.length data);
+    Printf.eprintf "bytes_sent %d" !bytes_sent
+  done;
+  close client_socket
+
 let () =
   (* Create a TCP server socket *)
+  let threads = ref [] in
   let server_socket = socket PF_INET SOCK_STREAM 0 in
   setsockopt server_socket SO_REUSEADDR true;
   bind server_socket (ADDR_INET (inet_addr_of_string "127.0.0.1", 6379));
   listen server_socket 1;
 
   (* Uncomment the code below to pass the first stage *)
-  let data = "+PONG\r\n" in
-  let buf = Bytes.create 32 in
-  let client_socket, _ = accept server_socket in
   while true do
-    let _ = read client_socket buf 0 16 in
-    let _ = write client_socket (Bytes.of_string data) 0 (String.length data) in
-    ()
-  done;
-  close client_socket;
-  close server_socket
+    let client_socket, _ = accept server_socket in
+    threads := Thread.create process_client client_socket :: !threads
+  done
+(* close server_socket *)
