0befeffa63bde7d5160a0981daa8668f40b79df0
Ruben Bakker <ruben@uncomplex.ch>
2025-12-03 16:23:39 +0100

    codecrafters submit [skip ci]

--- lib/ints.ml
+++ lib/ints.ml
@@ -5,7 +5,7 @@ let to_resp (value : t) : Resp.t = Resp.Integer value
 let set (value : t) (listener_count : int) (_existing : t option) :
     t Storeop.mutation_result =
   let notify_with = if listener_count > 0 then [ to_resp value ] else [] in
-  { store = Some value; return = Resp.BulkString "OK"; notify_with }
+  { store = Some value; return = Resp.SimpleString "OK"; notify_with }
 
 let incr (listener_count : int) (existing : t option) :
     t Storeop.mutation_result =
--- lib/strings.ml
+++ lib/strings.ml
@@ -7,4 +7,5 @@ let to_resp (value : t) : Resp.t = Resp.BulkString value
 let set (value : t) (listener_count : int) (_existing : t option) :
     t Storeop.mutation_result =
   let notify_with = if listener_count > 0 then [ to_resp value ] else [] in
-  { store = Some value; return = Resp.BulkString "OK"; notify_with }
+  { store = Some value; return = Resp.SimpleString "OK"; notify_with }
+
