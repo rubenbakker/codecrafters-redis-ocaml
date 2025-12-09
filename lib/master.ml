57fdabc8bbeeade25c67ad7c0a49cc16439bb50d
Ruben Bakker <ruben@uncomplex.ch>
2025-12-09 15:19:31 +0100

    Extended master

--- lib/master.ml
+++ lib/master.ml
@@ -1,4 +1,4 @@
-type slave_t = { socket : Unix.file_descr }
+type slave_t = { socket : Unix.file_descr; bytes_sent : int; bytes_ack : int }
 type t = { slaves : slave_t list }
 
 let state_lock = Stdlib.Mutex.create ()
@@ -14,17 +14,11 @@ let protect fn =
       lock ();
       fn ())
 
-let state : Unix.file_descr list ref = ref []
-
-let send_to_slave sock (payload : Resp.t) : Resp.t =
-  let payload = payload |> Resp.to_string |> Bytes.of_string in
-  ignore (Unix.write sock payload 0 (Bytes.length payload));
-  let buf = Bytes.create 512 in
-  ignore (Unix.read sock buf 0 512);
-  Resp.from_string (Bytes.to_string buf) 0
+let state : slave_t list ref = ref []
 
 let register_slave (socket : Unix.file_descr) (rdb : Resp.t option) : unit =
-  protect (fun () -> state := socket :: !state);
+  protect (fun () ->
+      state := { socket; bytes_sent = 0; bytes_ack = 0 } :: !state);
   match rdb with
   | Some rdb ->
       let rdb_string = Resp.to_string rdb in
@@ -36,9 +30,15 @@ let register_slave (socket : Unix.file_descr) (rdb : Resp.t option) : unit =
 
 let notify_slaves (command : Resp.t) : unit =
   protect (fun () ->
+      let new_list : slave_t list ref = ref [] in
       List.iter
-        (fun socket ->
+        (fun slave ->
           let result = Resp.to_string command in
+          let payload_length = String.length result in
           ignore
-            (Unix.write socket (Bytes.of_string result) 0 (String.length result)))
-        !state)
+            (Unix.write slave.socket (Bytes.of_string result) 0 payload_length);
+          new_list :=
+            { slave with bytes_sent = slave.bytes_sent + payload_length }
+            :: !new_list)
+        !state;
+      state := !new_list)
