75745db4bd11551c0643ca680f42d275bd5670c7
Ruben Bakker <ruben@uncomplex.ch>
2025-12-18 15:11:58 +0100

    codecrafters submit [skip ci]

--- .codecrafters/compile.sh
+++ .codecrafters/compile.sh
@@ -7,5 +7,5 @@
 # Learn more: https://codecrafters.io/program-interface
 
 set -e # Exit on failure
-opam install base base64 ppx_assert ppx_inline_test ppx_expect ppx_sexp_conv ppx_compare
+opam install base base64 ppx_assert ppx_inline_test ppx_expect ppx_sexp_conv ppx_compare digestif
 dune build --build-dir /tmp/codecrafters-build-redis-ocaml
